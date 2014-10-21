4da8ca060e1e4125a5d2da968059ff33a1d0f7fd
Michał Kalbarczyk <fazibear@gmail.com>
2014-10-21 23:33:50 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", function(){
+  var pl = document.getElementById('pl');
+  var en = document.getElementById('en');
+
+  var plLink = document.getElementById('pl-link');
+  var enLink = document.getElementById('en-link');
+
+  plLink.addEventListener("click", function(evt){
+    console.log('asd')
+    en.style.display = 'none';
+    pl.style.display = 'block';
+    evt.preventDefault();
+  });
+
+  enLink.addEventListener("click", function(evt){
+    pl.style.display = 'none';
+    en.style.display = 'block';
+    evt.preventDefault();
+  });
+  
+  en.style.display = 'block';
+});
