45de1cce2ee6b80b910883218d53f81894873002
Michał Kalbarczyk <fazibear@gmail.com>
2016-08-28 00:54:22 +0200

    Try to fix mobile

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
 ---
 layout: null
 ---
-document.querySelectorAll('a.blog-button').forEach(function(button, i){
-  panelCover = function(){
-    return document.querySelectorAll('.panel-cover')[0];
+var panelCover = function(){
+  return document.querySelectorAll('.panel-cover')[0];
+}
+
+var toggle = function(selector, className){
+  var el = document.querySelectorAll(selector)[0]
+  if (el.classList) {
+    el.classList.toggle(className);
+  } else {
+    var classes = el.className.split(' ');
+    var existingIndex = classes.indexOf(className);
+
+    if (existingIndex >= 0)
+      classes.splice(existingIndex, 1);
+    else
+      classes.push(className);
+
+    el.className = classes.join(' ');
   }
+}
+
+document.querySelectorAll('a.blog-button').forEach(function(button, i){
   button.addEventListener("click", function(e) {
     if(panelCover().classList.contains('panel-cover--collapsed')){
       return;
@@ -23,22 +41,22 @@ if (window.location.hash && window.location.hash == '#about') {
 
 document.querySelectorAll('.btn-mobile-menu').forEach(function(button, i){
   button.addEventListener("click", function(e) {
-    document.querySelectorAll('.navigation-wrapper')[0].classList.toggle('visible');
-    document.querySelectorAll('.navigation-wrapper')[0].classList.toggle('animated');
-    document.querySelectorAll('.navigation-wrapper')[0].classList.toggle('bounceInDown');
-    document.querySelectorAll('.btn-mobile-menu__icon')[0].classList.toggle('icon-list');
-    document.querySelectorAll('.btn-mobile-menu__icon')[0].classList.toggle('icon-x-circle');
-    document.querySelectorAll('.btn-mobile-menu__icon')[0].classList.toggle('animated');
-    document.querySelectorAll('.btn-mobile-menu__icon')[0].classList.toggle('fadeIn');
+    toggle('.navigation-wrapper', 'visible');
+    toggle('.navigation-wrapper', 'animated');
+    toggle('.navigation-wrapper', 'bounceInDown');
+    toggle('.btn-mobile-menu__icon', 'icon-list');
+    toggle('.btn-mobile-menu__icon', 'icon-x-circle');
+    toggle('.btn-mobile-menu__icon', 'animated');
+    toggle('.btn-mobile-menu__icon', 'fadeIn');
   });
 });
 
 document.querySelectorAll('.navigation-wrapper .blog-button').forEach(function(button, i){
   button.addEventListener("click", function(e) {
-    document.querySelectorAll('.navigation-wrapper')[0].classList.toggle('visible');
-    document.querySelectorAll('.btn-mobile-menu__icon')[0].classList.toggle('icon-list');
-    document.querySelectorAll('.btn-mobile-menu__icon')[0].classList.toggle('icon-x-circle');
-    document.querySelectorAll('.btn-mobile-menu__icon')[0].classList.toggle('animated');
-    document.querySelectorAll('.btn-mobile-menu__icon')[0].classList.toggle('fadeIn');
+    toggle('.navigation-wrapper', 'visible');
+    toggle('.btn-mobile-menu__icon', 'icon-list');
+    toggle('.btn-mobile-menu__icon', 'icon-x-circle');
+    toggle('.btn-mobile-menu__icon', 'animated');
+    toggle('.btn-mobile-menu__icon', 'fadeIn');
   });
 });
